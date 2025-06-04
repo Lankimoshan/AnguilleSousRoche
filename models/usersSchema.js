1048a3344098b43ffbb9b70b37a4021fc80ad41d
Lankimoshan <ScalianCTF@proton.me>
2025-06-04 18:43:36 +0200

    fix: clean fixed position

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -1,24 +1,24 @@
 const usersSchema = {
-  bsonType: "object",
-  required: ["username", "email", "createdAt"],
-  properties: {
-    _id: {
-      bsonType: "objectId"
-    },
-    username: {
-      bsonType: "string",
-      description: "must be a string and is required"
-    },
-    email: {
-      bsonType: "string",
-      pattern: "^.+@.+\\..+$",
-      description: "must be a valid email and is required"
-    },
-    createdAt: {
-      bsonType: "date",
-      description: "must be a date and is required"
+    bsonType: "object",
+    required: ["username", "email", "createdAt"],
+    properties: {
+        _id: {
+        bsonType: "objectId"
+        },
+        username: {
+        bsonType: "string",
+        description: "must be a string and is required"
+        },
+        email: {
+        bsonType: "string",
+        pattern: "^.+@.+\\..+$",
+        description: "must be a valid email and is required"
+        },
+        createdAt: {
+        bsonType: "date",
+        description: "must be a date and is required"
+        }
     }
-  }
 };
 
 module.exports = usersSchema;
