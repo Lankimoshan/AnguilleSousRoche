9980f3b1a00c331cee2811d5dc148eabff218c9c
Lankimoshan <ScalianCTF@proton.me>
2025-06-04 18:25:48 +0200

    feat : add objects shema

---
+++ models/weatherSchema.js
@@ -0,0 +1,27 @@
+const weatherSchema = {
+  bsonType: "object",
+  required: ["city", "temperature", "condition", "updatedAt"],
+  properties: {
+    _id: {
+      bsonType: "objectId"
+    },
+    city: {
+      bsonType: "string",
+      description: "city name, must be a string and is required"
+    },
+    temperature: {
+      bsonType: "double",
+      description: "temperature in Celsius, must be a double and is required"
+    },
+    condition: {
+      bsonType: "string",
+      description: "weather condition description, must be a string and is required"
+    },
+    updatedAt: {
+      bsonType: "date",
+      description: "last update date, must be a date and is required"
+    }
+  }
+};
+
+module.exports = weatherSchema;
